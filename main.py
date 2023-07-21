97b81bee3129956ed2e165f28c2c538a5c86ca8b
Gustavo Irineu <101259023+gustavoib@users.noreply.github.com>
2023-07-21 10:32:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ print("sobre trancamentos:\n")
 num_trancadas = int(input("número de disciplinas trancadas: "))
 
 soma_trancadas = 0
-for i in range(num_trancadas):
-  sem_trancadas = int(input(f"semestre da {i+1}ª disciplinas trancada: "))
-  carga_horaria_trancadas = int(input(f"carga horária da {i+1}ª disciplina trancada: "))
-  
-  soma_trancadas += (sem_trancadas*carga_horaria_trancadas)  
 
+if num_trancadas == 0:
+  carga_horaria_trancadas = 0
+  sem_trancadas = 0
+else:
+  for i in range(num_trancadas):
+    sem_trancadas = int(input(f"semestre da {i+1}ª disciplinas trancada: "))
+    carga_horaria_trancadas = int(input(f"carga horária da {i+1}ª disciplina trancada: "))
+  
+    soma_trancadas += (sem_trancadas*carga_horaria_trancadas)  
+  
 print("sobre as disciplinas:\n")
 n = int(input("disciplinas cursadas ou trancadas até o momento: "))
 
 C = 0
 nota_final = 0
 parcial = 0
-for i in range(n): 
- 
+for i in range(n):
   nota = float(input(f"nota da disciplina {i+1}: "))
   carga_horaria = float(input(f"carga horaria da disciplina {i+1}: "))
   semestre = float(input(f"semestre da disciplina {i+1}: "))
@@ -26,4 +30,4 @@ for i in range(n):
 
 ira = (1 - (0.5 * carga_horaria_trancadas) / C) * (nota_final / (parcial - soma_trancadas)) * 1000
 
-print("o ira individual do estudante é: ", ira)
+print("seu iraI eh: ", ira)
